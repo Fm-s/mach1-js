db11c17a127b04d2bac689647944f0ce99105085
Felipe Souza <felipe-_-br@hotmail.com>
2023-01-19 20:02:03 -0300

    aula3

---
+++ aula3/js.js
@@ -0,0 +1,13 @@
+let texto = "Manipulando strings com JS";
+
+alert(texto.toUpperCase());
+
+console.log(texto.slice(0,5));
+
+alert(texto.length);
+
+let texto2 = "Vamos concatenar as duas strings.";
+
+let texto3 = texto + texto2
+
+console.log(texto3);
