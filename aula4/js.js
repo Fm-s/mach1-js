74f4a7ee81f1b840f4188f06328f7bc1d2e1d87b
Felipe Souza <felipe-_-br@hotmail.com>
2023-01-24 21:14:23 -0300

    aula5

--- aula4/js.js
+++ aula4/js.js
@@ -1,14 +1,14 @@
 while(1){
-let value = prompt("Digite uma letra do alfabeto:").toLowerCase();
+    let value = prompt("Digite uma letra do alfabeto:").toLowerCase();
 
-if(+value || value === value.toUpperCase()){
-    alert("Deve digitar uma letra para obeter o resultado");
-}else if(value.length > 1){
-    alert("Só uma letra deve ser digitada");
-}else if(value === "a" || value === "e" 
-|| value === "i" || value === "o" || value === "u") {
-    alert("Letra digitada é vogal");
-}else {
-    alert("Letra digitada é consoante");
-}
+    if(+value || value === value.toUpperCase()){
+        alert("Deve digitar uma letra para obeter o resultado");
+    }else if(value.length > 1){
+        alert("Só uma letra deve ser digitada");
+    }else if(value === "a" || value === "e" 
+    || value === "i" || value === "o" || value === "u") {
+        alert("Letra digitada é vogal");
+    }else {
+        alert("Letra digitada é consoante");
+    }
 }
---
+++ aula5/js.js
@@ -0,0 +1,43 @@
+switch("oi"){
+    default:
+        console.log("default");
+        break;
+    case "oi":
+        console.log("Helo world!");
+    case "oioi":
+        console.log("oioi");
+}
+
+
+let pessoa = prompt("Digite seu nome:").toLowerCase();
+let animal = prompt("Digite seu animal").toLowerCase();
+
+let stringOutput = `Olá ${pessoa}, para o seu ${animal} oferecemos as melhores rações do mercado alem de`
+
+switch(animal){
+    case "cachorro":
+    case "gato":
+    case "coelho":
+        stringOutput += " banho e tosa com nossos especialistas!";
+    break;
+
+    case "cavalo":
+    case "cabra":
+    case "jumento":
+        stringOutput += " encarminhamos para os melhores veterinarios especialsitas do mercado!"
+    break;
+
+    case "rato":
+        stringOutput += " gaiolas especias!"
+    break;
+
+    case "passarinho":
+        stringOutput += " implates rastreaveis por GPS, os mais modernos do mercado!"
+    break;
+
+    default:
+        stringOutput = `Olá ${pessoa}, para o seu ${animal} infelizmente não temos nenhum serviço ou produto`
+
+}
+
+alert(stringOutput);
