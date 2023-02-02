df349af02dce87ec6c4029e3526f0e8330396936
Felipe Souza <felipe-_-br@hotmail.com>
2023-02-02 20:53:39 -0300

    aula 4

---
+++ Intermediario - aula 4/js.js
@@ -0,0 +1,30 @@
+
+const validaCpf = (cpf) => {
+    console.log("in")
+
+    if(isNaN(cpf)){
+        alert("Digite somente numeros!")
+        return false
+    }
+    
+    if(cpf.length != 11){
+        alert("Tamanho do CPF inválido")
+        return false
+    } 
+  
+    return true
+}
+
+const main = () => {
+    let cpf
+    do{
+        cpf = prompt("Digite um CPF (Somente Numeros")
+    } while(!validaCpf(cpf))
+    
+    cpf = `${cpf.slice(0,3)}.${cpf.slice(3,6)}.${cpf.slice(6,9)}-${cpf.slice(9,11)}`
+    alert(cpf)
+}
+
+
+main()
+
